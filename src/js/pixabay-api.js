6bde611ab85c0a569a2f4680f293c5c5068f340c
Oleksandr Liashkov <aleksandr.liashkov98@gmail.com>
2026-06-15 20:50:23 +0300

    Ready to check by mentor

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,17 @@
+import axios from 'axios';
+
+const API_KEY = '56322343-d60e20edbd0bfd9a78689fe35';
+
+export function getImagesByQuery(query) {
+  return axios
+    .get('https://pixabay.com/api/', {
+      params: {
+        key: API_KEY,
+        q: query,
+        image_type: 'photo',
+        orientation: 'horizontal',
+        safesearch: true,
+      },
+    })
+    .then(response => response.data);
+}
--- src/main.js
+++ src/main.js
@@ -1,9 +1,66 @@
-import iziToast from 'izitoast';
+// Импорт базовых стилей приложения
+import './css/reset.css';
+import './css/base.css';
 
+// Импорт библиотек
+import iziToast from 'izitoast';
 import 'izitoast/dist/css/iziToast.min.css';
 
-import SimpleLightbox from 'simplelightbox';
+// Импорт модулей
+import { getImagesByQuery } from './js/pixabay-api.js';
+import {
+  createGallery,
+  clearGallery,
+  showLoader,
+  hideLoader,
+} from './js/render-functions.js';
 
-import 'simplelightbox/dist/simple-lightbox.min.css';
+const form = document.querySelector('.form');
 
-// ----------------------------------------------------- //
+form.addEventListener('submit', handleSubmit);
+
+function handleSubmit(event) {
+  event.preventDefault();
+
+  const query = event.currentTarget.elements['search-text'].value.trim();
+
+  if (!query) {
+    iziToast.warning({
+      message: 'Please enter a search query!',
+      position: 'topRight',
+    });
+    return;
+  }
+
+  // Очищаем прошлый результат и запускаем лоадер
+  clearGallery();
+  showLoader();
+
+  getImagesByQuery(query)
+    .then(data => {
+      if (!data.hits || data.hits.length === 0) {
+        iziToast.error({
+          message:
+            'Sorry, there are no images matching your search query. Please try again!',
+          position: 'topRight',
+        });
+        return;
+      }
+
+      // Отрисовка галереи
+      createGallery(data.hits);
+    })
+    .catch(error => {
+      console.error(error);
+      iziToast.error({
+        message: 'Something went wrong. Please try again.',
+        position: 'topRight',
+      });
+    })
+    .finally(() => {
+      // Прячем лоадер в любом случае (успех или ошибка)
+      hideLoader();
+    });
+
+  form.reset();
+}
